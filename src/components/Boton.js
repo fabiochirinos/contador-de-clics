e79073eb77fc5fdc810163ca0005ae16bb46dc6c
Fabio <fabiochirinos7@gmail.com>
2022-11-03 16:57:37 -0500

    Se creo componente Boton

---
+++ src/components/Boton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Boton({ texto, esBotonDeClic, manejarClic }) {
+  return (
+    <button
+      className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+      onClick={manejarClic}>
+      {texto}
+    </button>
+  )
+}
